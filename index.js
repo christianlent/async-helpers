a7d2744483700f98c6bcdae5d9685d1e1b8e9e24
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-14 09:13:42 +0200

    fix: add source, update deps & eslint config
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- index.js
+++ index.js
@@ -7,3 +7,95 @@
 
 'use strict';
 
+const escapeRegex = require('escape-string-regexp');
+const isAsyncFunction = require('is-async-function');
+
+class AsyncHelpers {
+  constructor(options) {
+    this.options = Object.assign({}, options);
+    this.prefix = this.options.prefix || '{$ASYNCID$';
+    this.allHelpers = {};
+    this.counter = 0;
+    this.ids = {};
+  }
+
+  get(name) {
+    if (!name || typeof name !== 'string') {
+      return this.allHelpers;
+    }
+    return this.allHelpers[name];
+  }
+
+  helper(name, fn) {
+    if (!name || typeof name !== 'string') {
+      throw new TypeError('expect `name` to be non empty string');
+    }
+    if (!fn || typeof fn !== 'function') {
+      throw new TypeError('expect `fn` to be function');
+    }
+
+    const type = Object.prototype.toString.call(fn);
+    const isAsync = isAsyncFunction(fn);
+
+    // If it is an regular async function or callback one
+    // we are doing some magic, otherwise skip wrapping the normal ones.
+    if (type.includes('Async') || isAsync) {
+      this.counter += 1;
+      const id = `{$ASYNCID$${Date.now()}$${name}$${this.counter}`;
+
+      const func = isAsync ? promisify(fn) : fn;
+
+      this.allHelpers[name] = (...args) => {
+        this.ids[id] = { id, count: this.counter, name, args, fn: func };
+        return id;
+      };
+    } else {
+      this.allHelpers[name] = fn;
+    }
+
+    return this;
+  }
+
+  helpers(obj) {
+    if (obj && typeof obj === 'object' && !Array.isArray(obj)) {
+      Object.keys(obj).forEach((key) => {
+        this.helper(key, obj[key]);
+      });
+      return this;
+    }
+    throw new TypeError('expect `obj` to be object of helpers');
+  }
+
+  async resolve(str) {
+    const promises = Object.keys(this.ids)
+      .map((id) => this.ids[id])
+      .map(async({ id, args, fn }) => {
+        this.ids[id].value = await fn(...args);
+
+        return this.ids[id];
+      });
+
+    return Promise.all(promises).then((res) =>
+      res.reduce(
+        (acc, { id, value }) =>
+          acc.replace(new RegExp(escapeRegex(id), 'g'), value),
+        str,
+      ),
+    );
+  }
+}
+
+function promisify(fn) {
+  return (...args) =>
+    new Promise((resolve, reject) => {
+      fn(...args, (err, ...argz) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(...argz);
+        }
+      });
+    });
+}
+
+module.exports = (options) => new AsyncHelpers(options);
